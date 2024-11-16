182a76a026a02cb1defb8e3e330a285fd9407dfa
HimanshuSingh <himanshu.singh@allen.in>
2024-11-16 15:10:21 +0530

    limiting scope of PR to grpc connection code only

--- v1/pkg/httpwrapper/interface.go
+++
@@ -1,9 +0,0 @@
-package httpwrapper
-
-import (
-	"net/http"
-)
-
-type IHTTPClient interface {
-	MakeHttpCall(url, method string, payload map[string]interface{}, headers map[string]string, requestCookies []*http.Cookie) ([]byte, []*http.Cookie, error)
-}
--- v1/pkg/utils/color.go
+++
@@ -1,5 +0,0 @@
-package utils
-
-const (
-	WhiteColor = "#FFFFFF"
-)
